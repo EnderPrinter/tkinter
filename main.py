bfabcd39470c2bb659c81b0302f77c0cfed7ac78
Ender <ender3printer@mail.ru>
2022-03-16 01:46:01 +0500

    added automatic sending for seconds

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ import serial
 import array
 global voltage
 lst = [None] * 60
-ser = serial.Serial('COM7', 9600, timeout=0)
+ser = serial.Serial('COM5', 9600, timeout=0)
 window = Tk()
 text_box = Text(window)
 #text_box.pack()
@@ -34,6 +34,10 @@ window.geometry('840x640')
 txt = Entry(window,width=15)
 
 txt.grid(column=4, row=0)
+
+txtTime = Entry(window,width=15)
+
+txtTime.grid(column=5, row=0)
 
 def clicked():
     global voltage
@@ -70,6 +74,66 @@ def clicked():
     text_box.insert("1.0", "voltage: " + str(voltage) + "\n")
     text_box.grid(column=2, row=1)
 
+def timing():
+    res = txtTime.get()
+    res = res[:-1]
+    while True:
+        global voltage
+        ser.write(serial.to_bytes([0x55, 0x55, 0x00, 0x00, 0xAA]))
+        time.sleep(int(res))
+        for i in range(1, 60):
+
+            if (ser.in_waiting > 0):
+                serialString = ser.read()
+                lst[i] = None
+                lst[i] = serialString
+
+        print(lst)
+        filtered_list = list(filter(None, lst))
+        print(filtered_list)
+        result = filtered_list[35].hex() + filtered_list[34].hex()
+        print(filtered_list[35])
+        print(filtered_list[34])
+        print(result)
+        print("hex: " + filtered_list[35].hex() + " " + filtered_list[34].hex())
+        string_list = list(result)
+        if (string_list[0] == '0'):
+            string_list[0] = '0x'
+        else:
+            string_list[0] = '0x' + string_list[0]
+
+        hex_result = "".join(string_list)
+
+        an_integer = int(hex_result, 16)
+        hex_value = hex(an_integer)
+
+        hex_int = int(hex_value, 16)
+
+        if (str(filtered_list[37].hex()) == "08"):
+            voltage = hex_int / 10000
+        elif (str(filtered_list[37].hex()) == "04"):
+            voltage = hex_int / 1000
+        elif (str(filtered_list[37].hex()) == "00"):
+            voltage = hex_int
+        elif (str(filtered_list[37].hex()) == "02"):
+            voltage = hex_int / 100
+        elif (str(filtered_list[37].hex()) == "01"):
+            voltage = hex_int / 10
+
+        txt.delete(0, tkinter.END)
+        if (str(filtered_list[36].hex()) == '80'):
+            unit = filtered_list[38].hex()
+            text = str("-" + str(voltage) + str(range_dict[unit]))
+            txt.insert(0, text)
+        else:
+            unit = filtered_list[38].hex()
+            text = str(str(voltage) + str(range_dict[unit]))
+            txt.insert(0, text)
+        # filtered_list = 0
+
+        print(filtered_list)
+        print(lst)
+        print(voltage)
 
 def sended():
     global voltage
@@ -137,6 +201,10 @@ btn.grid(column=2, row=0)
 btn2 = Button(window, text="get value", command=sended)
 
 btn2.grid(column=3, row=0)
+
+btnTime = Button(window, text="timing", command=timing)
+
+btnTime.grid(column=6, row=0)
 
 window.mainloop()
 
