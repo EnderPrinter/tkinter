b93849ec98c0113e42c584d614b7638ccaf74bd5
Ender <ender3printer@mail.ru>
2022-03-18 01:05:37 +0500

    added threading

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+import threading
 range_dict = {
   '01': 'V',
   '02': 'mV',
@@ -74,18 +74,9 @@ def clicked():
     text_box.insert("1.0", "voltage: " + str(voltage) + "\n")
     text_box.grid(column=2, row=1)
 
-def timing():
-    res = txtTime.get()
-    unit = res[-1]
-    res = res[:-1]
-    print (unit)
-    print(res)
-    if (unit == "m"):
-        res = int(res) * 60
-        print (res)
-    elif (unit == "h"):
-        res = int(res) * 60 * 60
+def interrupt():
     while True:
+        global res
         global voltage
         ser.write(serial.to_bytes([0x55, 0x55, 0x00, 0x00, 0xAA]))
         time.sleep(int(res))
@@ -143,6 +134,81 @@ def timing():
         print(lst)
         print(voltage)
 
+
+
+def timing():
+
+    global res
+    res = txtTime.get()
+    unit = res[-1]
+    res = res[:-1]
+    print (unit)
+    print(res)
+    if (unit == "m"):
+        res = int(res) * 60
+        print (res)
+    elif (unit == "h"):
+        res = int(res) * 60 * 60
+    t = threading.Timer(int(res), interrupt, )
+    t.start()
+    # while True:
+    #     global voltage
+    #     ser.write(serial.to_bytes([0x55, 0x55, 0x00, 0x00, 0xAA]))
+    #     time.sleep(int(res))
+    #     for i in range(1, 60):
+    #
+    #         if (ser.in_waiting > 0):
+    #             serialString = ser.read()
+    #             lst[i] = None
+    #             lst[i] = serialString
+    #
+    #     print(lst)
+    #     filtered_list = list(filter(None, lst))
+    #     print(filtered_list)
+    #     result = filtered_list[35].hex() + filtered_list[34].hex()
+    #     print(filtered_list[35])
+    #     print(filtered_list[34])
+    #     print(result)
+    #     print("hex: " + filtered_list[35].hex() + " " + filtered_list[34].hex())
+    #     string_list = list(result)
+    #     if (string_list[0] == '0'):
+    #         string_list[0] = '0x'
+    #     else:
+    #         string_list[0] = '0x' + string_list[0]
+    #
+    #     hex_result = "".join(string_list)
+    #
+    #     an_integer = int(hex_result, 16)
+    #     hex_value = hex(an_integer)
+    #
+    #     hex_int = int(hex_value, 16)
+    #
+    #     if (str(filtered_list[37].hex()) == "08"):
+    #         voltage = hex_int / 10000
+    #     elif (str(filtered_list[37].hex()) == "04"):
+    #         voltage = hex_int / 1000
+    #     elif (str(filtered_list[37].hex()) == "00"):
+    #         voltage = hex_int
+    #     elif (str(filtered_list[37].hex()) == "02"):
+    #         voltage = hex_int / 100
+    #     elif (str(filtered_list[37].hex()) == "01"):
+    #         voltage = hex_int / 10
+    #
+    #     txt.delete(0, tkinter.END)
+    #     if (str(filtered_list[36].hex()) == '80'):
+    #         unit = filtered_list[38].hex()
+    #         text = str("-" + str(voltage) + str(range_dict[unit]))
+    #         txt.insert(0, text)
+    #     else:
+    #         unit = filtered_list[38].hex()
+    #         text = str(str(voltage) + str(range_dict[unit]))
+    #         txt.insert(0, text)
+    #     # filtered_list = 0
+    #
+    #     print(filtered_list)
+    #     print(lst)
+    #     print(voltage)
+    #
 def sended():
     global voltage
     ser.write(serial.to_bytes([0x55, 0x55, 0x00, 0x00, 0xAA]))
